4c2d5a5af650e6af12145f38a529e4ac0cffebfe
Alex Whitman <alex@alexwhitman.com>
2011-11-18 16:24:15 +0000

    Remove bot.Module.load() - no longer required
    - options are now assigned to module rather than passed in

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 module.exports = function(bot, module) {
 
-	module.load = function() {
-		var replies = [
-			'It is certain',
-			'It is decidedly so',
-			'Without a doubt',
-			'Yes – definitely',
-			'You may rely on it',
-			'As I see it, yes',
-			'Most likely',
-			'Outlook good',
-			'Signs point to yes',
-			'Yes',
-			'Reply hazy, try again',
-			'Ask again later',
-			'Better not tell you now',
-			'Cannot predict now',
-			'Concentrate and ask again',
-			'Don\'t count on it',
-			'My reply is no',
-			'My sources say no',
-			'Outlook not so good',
-			'Very doubtful'
-		];
+	var replies = [
+		'It is certain',
+		'It is decidedly so',
+		'Without a doubt',
+		'Yes – definitely',
+		'You may rely on it',
+		'As I see it, yes',
+		'Most likely',
+		'Outlook good',
+		'Signs point to yes',
+		'Yes',
+		'Reply hazy, try again',
+		'Ask again later',
+		'Better not tell you now',
+		'Cannot predict now',
+		'Concentrate and ask again',
+		'Don\'t count on it',
+		'My reply is no',
+		'My sources say no',
+		'Outlook not so good',
+		'Very doubtful'
+	];
 
-		module.addCommand(/(eightball|8ball) (.*)/i, function(request) {
-				request.reply = replies[Math.floor(Math.random()*replies.length)];
-				bot.respond(request);
-		});
-	};
+	module.addCommand(/(eightball|8ball) (.*)/i, function(request) {
+			request.reply = replies[Math.floor(Math.random()*replies.length)];
+			bot.respond(request);
+	});
 
 };
